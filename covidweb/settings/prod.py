a7fb87a37231992456c9f694bd2074e0ae43e4f2
Maxat Kulmanov <coolmaksat@gmail.com>
2020-05-14 09:47:41 +0300

    logging

--- covidweb/settings/prod.py
+++ covidweb/settings/prod.py
@@ -1,3 +1,22 @@
 from .base import *
 
 DEBUG = False
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'production.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
