d87579039772bc16ba2b3b8e6676eba3072033d2
Maxat Kulmanov <coolmaksat@gmail.com>
2020-12-29 10:03:39 +0300

    create uploader project in arvados for users

--- covidweb/apps/accounts/models.py
+++ covidweb/apps/accounts/models.py
@@ -4,6 +4,10 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save, pre_save
 from django.utils.translation import ugettext as _
 from django.core.exceptions import ValidationError
+from uploader.utils import api
+from django.conf import settings
+
+UPLOADER_PROJECT_UUID = getattr(settings, 'UPLOADER_PROJECT_UUID', 'cborg-j7d0g-nyah4ques5ww7pk')
 
 
 def check_unique_email(sender, instance, **kwargs):
@@ -19,14 +23,21 @@ pre_save.connect(check_unique_email, sender=User)
 class UserProfile(models.Model):
     user = models.OneToOneField(
         User, primary_key=True, on_delete=models.CASCADE)
-    organization = models.CharField(max_length=255)
+    organization = models.CharField(max_length=255, blank=True, null=True)
     token = models.CharField(max_length=127, blank=True, null=True, unique=True)
     project_uuid = models.CharField(max_length=31, blank=True, null=True)
 
 
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        UserProfile.objects.create(
+        project = api.groups().create(body={
+            "group_class": "project",
+            "name": instance.username,
+            "owner_uuid": UPLOADER_PROJECT_UUID,
+        }, ensure_unique_name=True).execute()
+        profile, created = UserProfile.objects.get_or_create(
             user=instance)
+        profile.project_uuid = project['uuid']
+        profile.save()
 
 post_save.connect(create_user_profile, sender=User)
